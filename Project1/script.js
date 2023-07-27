496cdb8185a6feaf866907dbd9b4a36168dd4ca0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-27 11:29:57 -0600

    --Values and Variables Overview

--- Project1/script.js
+++ Project1/script.js
@@ -1,8 +1,18 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 
 console.log(40 + 8 + 23 - 10);
 
-let firstName = "Jonas";
+let firstName = "Help Me";
 
-console.log(firstName)
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+
+let $function = 27;
+
+let person = 'Jonas'
+
+let job1 = 'programmer';
+let job2 = 'teacher';
